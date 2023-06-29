5d42da45c6c46112efd97384716cae9a06dc3e29
CaptHua <capthua24@gmail.com>
2023-06-29 18:04:54 +0800

    feature: initialize project structure

---
+++ common/src/main/java/io/visualjava/common/ClassUtil.java
@@ -0,0 +1,14 @@
+package io.visualjava.common;
+
+import java.util.List;
+
+/**
+ * @author CaptHua
+ */
+public class ClassUtil {
+
+    List<Class<?>> getClasses(Class<?> type){
+        return null;
+    }
+
+}
---
+++ testapp/src/main/java/io/visualjava/VisualJavaApplication.java
@@ -0,0 +1,13 @@
+package io.visualjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualJavaApplication.class, args);
+	}
+
+}
